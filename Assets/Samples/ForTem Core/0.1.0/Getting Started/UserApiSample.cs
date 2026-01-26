47937e760c67c25afe5dc6262eda56b2204ed87d
Colt Bauman <colt@fortemlabs.com>
2026-01-26 14:33:19 +0900

    sample: display user and item images

--- Assets/Samples/ForTem Core/0.1.0/Getting Started/UserApiSample.cs
+++ Assets/Samples/ForTem Core/0.1.0/Getting Started/UserApiSample.cs
@@ -11,6 +11,10 @@ namespace ForTemSdk.Samples
         [SerializeField] private TMP_InputField _walletAddressInput;
         [SerializeField] private Button _getUserInfoBtn;
 
+        [Header("Item Image")]
+        [SerializeField] private Image _profileImage;
+        [SerializeField] private UrlToUIImage _urlToUIImage;
+
         [Header("ForTem")]
         [SerializeField] private ForTemClientProvider _forTemClientProvider;
 
@@ -26,6 +30,10 @@ namespace ForTemSdk.Samples
                 var forTemClient = await _forTemClientProvider.GetClient();
                 var result = await forTemClient.UserApi.GetUser(_walletAddressInput.text);
                 Debug.Log($"User: {JsonUtility.ToJson(result, true)}");
+                if (!string.IsNullOrEmpty(result.ProfileImage))
+                {
+                    _urlToUIImage.SetImageFromUrl(_profileImage, result.ProfileImage);
+                }
             }
             catch (System.Exception ex)
             {
